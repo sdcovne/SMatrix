3d9b509878a71b59a4a7826d67316f648cb63eb1
Sebastiano Davide Covone <sebastiano.davide26@gmail.com>
2018-10-15 22:45:49 +0200

    Install command for Linux

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo su
+
+python3 -m venv venv && source venv/bin/activate
+
+pip3 install -r source/requirements/requirements.txt
+
+pip3 install --upgrade setuptools
+
+pyinstaller --onefile --windowed --name SMatrix source/smatrix.py
+
+mv  dist/SMatrix  /usr/local/bin/
+
+mv  smatrix.desktop  /usr/share/applications/
